a6746d5d5ad3a2aa4675adc1f3da844b97274507
unknown <khandvemukesh@gmail.com>
2024-11-27 13:55:12 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dontenv').config({path:'./env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./contents.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './contents';
+// import express from 'express';
+// const app =express();
+//(async ()=>{
+//     try{
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error)=>{
+//             console.log("ERROR:", error);
+//             throw error;
+//         });
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App Is Listen on Port ${process.env.PORT}`);
+//         })
+//     }catch(error){
+//         console.log("Error", error);
+//         throw error;
+//     }
+// })();
